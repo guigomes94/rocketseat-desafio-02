5247d394cf27f5ab065a10880c2de27fdc62c65a
Guilherme <guigomes94@outlook.com>
2019-12-21 13:46:58 -0300

    desafio02

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,84 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { name, email, age, weight, height } = req.body;
+
+    const student = await Student.findByPk(req.params.id);
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exist!' });
+      }
+    }
+
+    const { id } = await Student.findByPk(req.params.id);
+
+    await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get();
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
